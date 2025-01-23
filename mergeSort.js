cfbdd54ed79a12320d8bebe3677d56a54d9d715c
Robert DeLucia Jr. <bdelucia@asu.edu>
2025-01-23 14:47:21 -0700

    merge sort!

---
+++ mergeSort.js
@@ -0,0 +1,32 @@
+function mergeSort(array) {
+  if (array.length === 1) return array;
+  let middleIndex = array.length / 2;
+
+  let leftArray = array.slice(0, middleIndex);
+  let rightArray = array.slice(middleIndex);
+
+  let sortedLeftArray = mergeSort(leftArray);
+  let sortedRightArray = mergeSort(rightArray);
+
+  return merge(sortedLeftArray, sortedRightArray);
+}
+
+function merge(leftArray, rightArray) {
+  let mergedArray = [];
+  let i = 0,
+    j = 0;
+
+  while (i < leftArray.length && j < rightArray.length) {
+    if (leftArray[i] > rightArray[j]) {
+      mergedArray.push(rightArray[j]);
+      j++;
+    } else {
+      mergedArray.push(leftArray[i]);
+      i++;
+    }
+  }
+
+  return mergedArray.concat(leftArray.slice(i)).concat(rightArray.slice(j));
+}
+
+console.log(mergeSort([5, 3, 1, 4, 2]));
