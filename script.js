bd8592242da425f712fe49b08d9bfbb48117b6ee
Robert DeLucia Jr. <bdelucia@asu.edu>
2025-01-23 13:33:49 -0700

    fibo recursive

--- script.js
+++ script.js
@@ -12,4 +12,19 @@ function fibs(num) {
   return result;
 }
 
+function fibsRec(num) {
+  if (num <= 0) return [];
+  if (num === 1) return [0];
+  if (num === 2) return [0, 1];
+
+  const sequence = fibsRec(num - 1);
+
+  const nextNumber =
+    sequence[sequence.length - 1] + sequence[sequence.length - 2];
+  sequence.push(nextNumber);
+
+  return sequence;
+}
+
 console.log(fibs(8));
+console.log(fibsRec(8));
